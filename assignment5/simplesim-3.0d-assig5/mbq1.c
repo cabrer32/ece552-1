c3b641e3dd7b93e58a965cc6074b047e745d3e6b
tarafdar <tarafdar12@gmail.com>
2013-11-14 11:21:10 -0500

    mbq1

--- assignment5/simplesim-3.0d-assig5/mbq1.c
+++ assignment5/simplesim-3.0d-assig5/mbq1.c
@@ -1,20 +1,33 @@
 #define LOOPS 800000
+#define LOOPS_2 320000
 
 int main(){
     
     int i;
     int array[LOOPS];
+    int array_2[LOOPS_2];
     int sum = 0;
-    array[0]=0;
-    for(i=1;i<LOOPS;i++){
+    
+    
+    //since we are accessing all elements of this array contigiously
+    //the prefetching should allow for a 0 percent miss rate in our cache
+    for(i=0;i<LOOPS;i++){
         array[i]=i;
     }
+    
+    //since we are accessing all elements of this array with a stride size of 32 ints (2 cache lines)
+    //the prefetching will not help and we should have a cache miss every 1/32 times
+    for(i=0;i<LOOPS_2;i+=32){
+        array_2[i] = i; 
 
-  //  for(i=0;i<LOOPS;i++){
-  //      sum+=array[i];
-  //  }
+    }
+    
 
-    return array[LOOPS -1];
+    //expected total misses, the first loop 0 misses, the second loop 1/32*320000 = 10000 misses
+    
+    //return array[LOOPS - 1];
+    return array[LOOPS -1] + array_2[LOOPS_2 - 1];
+    //return array_2[LOOPS_2 - 1];
 
 }
 
